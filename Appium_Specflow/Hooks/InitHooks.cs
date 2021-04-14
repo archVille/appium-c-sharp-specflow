5ff3c25725649da0dcd86d781f68c153e0b6bba3
Panos M <PM>
2021-04-14 17:26:49 +0100

    hooks

---
+++ Appium_Specflow/Hooks/InitHooks.cs
@@ -0,0 +1,26 @@
+﻿using Appium_Specflow.Drivers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Appium_Specflow.Hooks
+{
+    [Binding]
+    class InitHooks
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public InitHooks(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [BeforeScenario]
+        public void Init()
+        {
+            AppiumDriver driver = new AppiumDriver();
+            driver.InitAppium();
+        }
+    }
+}
---
+++ Appium_Specflow/Steps/AppiumSteps.cs
@@ -0,0 +1,32 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Appium_Specflow.Steps
+{
+    [Binding]
+    public sealed class AppiumSteps
+    {
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public AppiumSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"I enter email and password")]
+        public void EnterEmailAndPassword(Table table)
+        {
+        }
+
+        [Given(@"I select the currency drop down")]
+        public void SelectTheCurrencyDropDown()
+        {
+        }
+
+        [Then(@"I verify the currency type")]
+        public void VerifyTheCurrencyType()
+        {
+        }
+
+    }
+}
